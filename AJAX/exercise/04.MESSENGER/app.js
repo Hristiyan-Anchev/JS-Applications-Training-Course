3562db92636015a148aafb1c9009dc1e483b1e21
Hristiyan-Anchev <hristian.anchev@gmail.com>
2020-07-04 12:13:59 +0100

    fixed code formatting

--- AJAX/exercise/04.MESSENGER/app.js
+++ AJAX/exercise/04.MESSENGER/app.js
@@ -9,20 +9,20 @@ function attachEvents() {
     let url = "https://rest-messanger.firebaseio.com/messanger.json";
 
     send.addEventListener("click", sendMessage);
-    refresh.addEventListener("click",displayMsg);
-    clear.addEventListener("click",clearChat);
+    refresh.addEventListener("click", displayMsg);
+    clear.addEventListener("click", clearChat);
 
-    function clearChat(){
+    function clearChat() {
         let promptSlot = document.querySelector("#prompt");
         let msgWindow = document.createElement("div");
         let buttons = document.querySelector(".buttons");
-        msgWindow.setAttribute("class","msg-window");
+        msgWindow.setAttribute("class", "msg-window");
         msgWindow.appendChild(document.createTextNode("You are about to delete the entire chat history!\n"));
         msgWindow.appendChild(document.createTextNode("Are you sure?"));
         let yesBtn = document.createElement("input");
         yesBtn.type = "button";
         yesBtn.value = "YES";
-        yesBtn.setAttribute("id","clear");
+        yesBtn.setAttribute("id", "clear");
         let noBtn = document.createElement("input");
         noBtn.type = "button";
         noBtn.value = "NO";
@@ -31,21 +31,21 @@ function attachEvents() {
         btnHolder.appendChild(yesBtn);
         btnHolder.appendChild(noBtn);
         msgWindow.appendChild(btnHolder);
-        noBtn.addEventListener("click",function(){
+        noBtn.addEventListener("click", function () {
             promptSlot.removeChild(msgWindow);
             Array.from(buttons.children).forEach(b => {
                 b.disabled = false;
             });
         });
-        yesBtn.addEventListener("click",function () {
+        yesBtn.addEventListener("click", function () {
             Array.from(buttons.children).forEach(b => {
                 b.disabled = false;
             });
-            fetch(url,{
-                method:"DELETE"
+            fetch(url, {
+                method: "DELETE"
             }).then(response => {
-                if(response.ok){
-                    console.log("Deleted: ",response.statusText);
+                if (response.ok) {
+                    console.log("Deleted: ", response.statusText);
                 }
             });
             chatArea.textContent = "";
@@ -57,45 +57,39 @@ function attachEvents() {
         Array.from(buttons.children).forEach(b => {
             b.disabled = true;
         });
-
-
-
-
-
-
     }
 
-    function displayMsg (){
+    function displayMsg() {
         chatArea.textContent = "";
         fetch("https://rest-messanger.firebaseio.com/messanger.json")
             .then(response => {
-                if(response.ok){
-                    console.log("great success: ",response.statusText);
+                if (response.ok) {
+                    console.log("great success: ", response.statusText);
                     return response.json();
                 }
             }).then(data => {
-                if(data){
+            if (data) {
                 let chatContent = [];
-                Object.entries(data).forEach(entry =>{
+                Object.entries(data).forEach(entry => {
                     chatContent.push(`${entry[1].author}: ${entry[1].content}`);
                 });
                 chatArea.textContent = chatContent.join("\n");
-                }
+            }
         });
     }
 
-    function sendMessage(evt){
+    function sendMessage(evt) {
         let msg = {
             author: name.value,
             content: message.value
         }
-        fetch(url,{
-            method:"POST",
-            headers:{"Content-Type":"application/json"},
+        fetch(url, {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify(msg)
         })
             .then(res => {
-                if(res.ok){
+                if (res.ok) {
                     displayMsg();
                     console.log("great success: ", res.statusText);
                 }
@@ -103,11 +97,6 @@ function attachEvents() {
 
         message.value = "";
     }
-
-
-
-
-
 }
 
 attachEvents();
